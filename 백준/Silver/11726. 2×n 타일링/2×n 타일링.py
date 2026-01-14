8e91060e6e31c485a32acb068b96cbe8b55f15be
lmmh202 <peterlee11@naver.com>
2026-01-14 16:48:27 +0900

    Add omited solution

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+fib = [0] * (n + 1)
+fib[1] = 1
+
+if n >= 2:
+    fib[2] = 2
+
+if n <= 2:
+    print(fib[n])
+else:
+    for i in range(3, n+1):
+        fib[i] = fib[i-1] + fib[i-2]
+    print(fib[n] % 10007)
---
+++ 백준/Silver/11726. 2×n 타일링/README.md
@@ -0,0 +1,3 @@
+# [Silver III] 2×n 타일링
+
+[문제 링크](https://www.acmicpc.net/problem/112726)
